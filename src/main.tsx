009fe3c43768b996add9700b0b622efd0326dcca
Qydnama <amangikbaktibaiuli@gmail.com>
2024-12-10 01:34:23 +0500

    third

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://raw.githubusercontent.com/Qydnama/first_contract_front_end/refs/heads/main/public/manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
